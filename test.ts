a22b786614b066f05fa413f74ada2c837cbbed07
Pig Fang <g-plane@hotmail.com>
2020-05-21 23:36:59 +0800

    headless

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import test from 'ava'
 import { launch } from 'puppeteer'
 
 test('visit home page', async (t) => {
-  const browser = await launch({ headless: false })
+  const browser = await launch()
   const page = await browser.newPage()
   await page.goto('https://littleskin.cn/')
 
